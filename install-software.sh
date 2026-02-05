a10f746b4f9d715ee0b8db73bb936304a703e271
Sergey Shumakov <79650499+sergshumakov@users.noreply.github.com>
2026-02-05 22:16:19 +0300

    Update install-software.sh

--- install-software.sh
+++ install-software.sh
@@ -1,8 +1,14 @@
 # Install Homebrew
-# bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install Oh-my-zsh
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install VPN Client
+brew install passepartout
+
+echo "Press enter after starting VPN"
+read
 
 # Install formulas
 brew install neovim tmux htop uv bat ripgrep node minio-mc wrk
