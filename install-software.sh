440f3897374151c0aa2152c97f3ca0e44bc791ed
Sergey Shumakov <sergey@shumakov.email>
2026-02-28 15:29:16 +0300

    replace iterm2 to ghostty

--- install-software.sh
+++ install-software.sh
@@ -11,7 +11,7 @@ read
 brew install neovim tmux htop uv bat ripgrep node minio-mc wrk
 
 # Install casks
-brew install pycharm webstorm chatgpt 1password telegram orbstack codex iterm2 arc tableplus ledger-wallet figma yandex raycast codex-app
+brew install pycharm webstorm chatgpt 1password telegram orbstack codex ghostty arc tableplus ledger-wallet figma yandex raycast codex-app
 
 # Install fonts
 brew install font-jetbrains-mono-nerd-font 
@@ -19,8 +19,8 @@ brew install font-jetbrains-mono-nerd-font
 # Install Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Install iTerm2 Config
-ln -s $HOME/.dotfiles/iterm2/com.googlecode.iterm2.plist $HOME/Library/Preferences/com.googlecode.iterm2.plist 
+# Install Ghostty config
+ln -s $HOME/.dotfiles/ghostty.config $HOME/.config/ghostty/config
 
 # Git config
 ln -s $HOME/.dotfiles/git/.gitconfig $HOME/.gitconfig
